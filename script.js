924137e25de52707786453415ee130693c4eabee
Hao Nguyen <noah@Haos-MacBook-Pro.local>
2026-04-19 15:15:10 -0700

    updatednavbar

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const logo = "myImages/OIP-4278803273.jpg";
-document.getElementById("logo").src = logo;
+window.onload = function() {
+    const navPlaceholder = document.getElementById('nav-placeholder');
+    if (navPlaceholder) {
+        fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                navPlaceholder.innerHTML = data;
+                const logo = "myImages/OIP-4278803273.jpg";
+                document.getElementById("logo").src = logo;
+            })
+            .catch(err => console.error("Could not load navbar:", err));
+    }
+};
+
+
