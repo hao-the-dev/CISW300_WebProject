271a464b97715660faf31f53f3b904cd11741949
Hao Nguyen <noah@Haos-MacBook-Pro.local>
2026-04-20 11:41:44 -0700

    AlmostDoneWithCloudServer

--- script.js
+++ script.js
@@ -12,4 +12,39 @@ window.onload = function() {
     }
 };
 
+// Supabase client setup //
+const SUPABASE_URL = 'https://zrgbbcrbmlpbwziyfjhn.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_ZvMlt5toiMdUOn6fEmRAWQ_KuWmgFnw';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const mentorshipForm = document.getElementById('mentorship-form');
 
+async function mentorshipFormSubmit(event) {
+    event.preventDefault(); // This is the most important line!
+    // Grab the values from your inputs
+    const nameValue = document.getElementById('name').value;
+    const emailValue = document.getElementById('email').value;
+    const addressValue = document.getElementById('address').value;
+    const countyValue = document.getElementById('county').value;
+    const cityValue = document.getElementById('city').value;
+    const zipValue = document.getElementById('zip').value;
+
+    // Send the data to your "form_submissions" table
+    const { data, error } = await supabaseClient
+        .from('mentorshipForm') 
+        .insert([{ 
+            Name: nameValue,
+            Email: emailValue,
+            Address: addressValue,
+            County: countyValue,
+            City: cityValue,
+            Zipcode: zipValue
+        }]);
+
+    if (error) {
+        console.error("Error sending to cloud:", error.message);
+        alert("Upload failed.");
+    } else {
+        alert("Successfully saved to Supabase!");
+        event.target.reset(); // Clears the form fields
+    }
+}
